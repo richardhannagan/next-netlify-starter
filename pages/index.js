c948f48ecd1d6f2e8deacbd6cf11d11727168db7
richardhannagan <hannagan@me.com>
2021-12-23 13:54:20 +1100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,10 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>international waters funk</title>
+		<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="/favicon.ico" type="image/x-icon">
+
       </Head>
 
       <main>
